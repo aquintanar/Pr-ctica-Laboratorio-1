d21e6bb92bb521bd2e0b5efac4de4aaece9a2f9f
ubuntu20 <a20191876@pucp.edu.pe>
2021-09-07 15:29:22 -0500

    Ejercicio con C

---
+++ Ejercicio3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+extern int Ejercicio3asm(int N,int*numeros);
+
+int main (int argc,char **argv){
+
+    int N;
+    int suma_total;
+    int *numeros;
+    N=argc-1;
+
+    numeros = (int*)malloc(N*sizeof(int));
+
+    for(int i=0;i<N;i++){
+        numeros[i]=(int)atoi(argv[i+1]);
+    }
+
+
+    suma_total=Ejercicio3asm(N,numeros);
+
+    printf("La suma de :");
+    for(int i=0;i<N;i++){
+        printf("%d ",numeros[i]);
+    }
+    printf("\n es : %d\n",suma_total);
+    
+    
+    return 0;
+}
---
+++ Ejercicio3asm.asm
@@ -0,0 +1,48 @@
+
+section .text
+
+    global Ejercicio3asm.asm
+
+Ejercicio3asm:
+
+    ;Prologo para calling conventions en 32 bits
+    push ebp
+    mov ebp, esp
+    push esi 
+    push edi 
+    push ebx
+
+    ;Limpiamos los registros
+    xor eax,eax
+    xor ebx,ebx
+    xor ecx,ecx
+    xor edx,edx
+
+    ;Utilizamos los punteros
+    mov edi,[ebp+12]  ; edi <-- *arr
+    mov ecx,[ebp+8]   ; ecx <--N
+
+    ;Algoritmo
+
+    mov eax , [edi]
+    mov ebx , ecx 
+    dec ecx 
+    jz exit
+
+    suma:
+        add edi,4
+        add eax ,[edi]
+        loop suma
+
+
+    exit:
+
+    ;Epilogo
+
+    pop ebx
+    pop edi
+    pop esi
+    pop esp, ebp
+    pop ebp
+    
+    ret
